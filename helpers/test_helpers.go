050c9952870262b4aed58351a30ea1018bb355f3
Jonathan Berkhahn <jaberkha@us.ibm.com>
2017-08-28 13:59:25 -0700

    Revert "Added tests for XFCC in the new http_routing package"
    This reverts commit 76f9d09d82922df15224bd1e9f0db8d2e844ee7c.

--- helpers/test_helpers.go
+++ helpers/test_helpers.go
@@ -1,15 +1,8 @@
 package helpers
 
 import (
-	"crypto/rand"
-	"crypto/rsa"
-	"crypto/x509"
-	"crypto/x509/pkix"
 	"encoding/json"
-	"encoding/pem"
 	"fmt"
-	"math/big"
-	"net"
 	"os"
 	"time"
 
@@ -24,8 +17,6 @@ import (
 	cfworkflow_helpers "github.com/cloudfoundry-incubator/cf-test-helpers/workflowhelpers"
 	"github.com/nu7hatch/gouuid"
 
-	"strings"
-
 	. "github.com/onsi/gomega"
 )
 
@@ -38,7 +29,6 @@ type RoutingConfig struct {
 	TcpAppDomain      string       `json:"tcp_apps_domain"`
 	LBConfigured      bool         `json:"lb_configured"`
 	TCPRouterGroup    string       `json:"tcp_router_group"`
-	Xfcc              XFCC         `json:"xfcc"`
 }
 
 type OAuthConfig struct {
@@ -46,15 +36,6 @@ type OAuthConfig struct {
 	ClientName    string `json:"client_name"`
 	ClientSecret  string `json:"client_secret"`
 	Port          int    `json:"port"`
-}
-
-type XFCC struct {
-	AlwaysForward []string `json:"always_forward"`
-	Forward       []string `json:"forward"`
-	SanitizeSet   []string `json:"sanitize_set"`
-	ClientPem     string   `json:"client_pem"`
-	CertPEM       []byte
-	KeyPEM        []byte
 }
 
 func loadDefaultTimeout(conf *RoutingConfig) {
@@ -94,51 +75,7 @@ func LoadConfig() RoutingConfig {
 
 	loadedConfig.RoutingApiUrl = fmt.Sprintf("https://%s", loadedConfig.ApiEndpoint)
 
-	loadedConfig.loadXFCC()
-
 	return loadedConfig
-}
-
-func (c *RoutingConfig) loadXFCC() {
-	certPEM, keyPEM := parsePEMBlocks([]byte(c.Xfcc.ClientPem))
-	c.Xfcc.CertPEM = certPEM
-	c.Xfcc.KeyPEM = keyPEM
-}
-
-func parsePEMBlocks(pemBlocks []byte) (certPEMBlock, keyPEMBlock []byte) {
-	for {
-		var block *pem.Block
-		block, pemBlocks = pem.Decode(pemBlocks)
-		if block == nil {
-			break
-		}
-
-		if isCertificate(block) {
-			certPEM := pem.EncodeToMemory(block)
-			certPEMBlock = append(certPEMBlock, certPEM...)
-		} else if isPrivateKey(block) {
-			keyPEM := pem.EncodeToMemory(block)
-			keyPEMBlock = append(keyPEMBlock, keyPEM...)
-		} else if isECParameters(block) {
-			continue
-		} else {
-			panic(fmt.Sprintf("error parsing router.tls_pem, found unsupported PEM block:\n%s", pem.EncodeToMemory(block)))
-		}
-	}
-
-	return
-}
-
-func isECParameters(block *pem.Block) bool {
-	return strings.Contains(block.Type, "PARAMETERS")
-}
-
-func isPrivateKey(block *pem.Block) bool {
-	return strings.Contains(block.Type, "PRIVATE KEY")
-}
-
-func isCertificate(block *pem.Block) bool {
-	return strings.Contains(block.Type, "CERTIFICATE")
 }
 
 func ValidateRouterGroupName(context cfworkflow_helpers.UserContext, tcpRouterGroup string) {
@@ -219,48 +156,4 @@ func RandomName() string {
 	}
 
 	return guid.String()
-}
-
-func CreateCertDER(cname string) (*rsa.PrivateKey, []byte) {
-	// generate a random serial number (a real cert authority would have some logic behind this)
-	serialNumberLimit := new(big.Int).Lsh(big.NewInt(1), 128)
-	serialNumber, err := rand.Int(rand.Reader, serialNumberLimit)
-	Expect(err).ToNot(HaveOccurred())
-
-	subject := pkix.Name{Organization: []string{"xyz, Inc."}}
-	if cname != "" {
-		subject.CommonName = cname
-	}
-
-	tmpl := x509.Certificate{
-		SerialNumber:          serialNumber,
-		Subject:               subject,
-		SignatureAlgorithm:    x509.SHA256WithRSA,
-		NotBefore:             time.Now(),
-		NotAfter:              time.Now().Add(time.Hour), // valid for an hour
-		BasicConstraintsValid: true,
-		IPAddresses:           []net.IP{net.ParseIP("127.0.0.1")},
-		KeyUsage:              x509.KeyUsageCertSign | x509.KeyUsageDigitalSignature,
-		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth, x509.ExtKeyUsageClientAuth},
-		IsCA:                  true,
-	}
-
-	privKey, err := rsa.GenerateKey(rand.Reader, 2048)
-	Expect(err).ToNot(HaveOccurred())
-	certDER, err := x509.CreateCertificate(rand.Reader, &tmpl, &tmpl, &privKey.PublicKey, privKey)
-	Expect(err).ToNot(HaveOccurred())
-	return privKey, certDER
-}
-
-func CreateKeyPair(cname string) (keyPEM, certPEM []byte) {
-	privKey, certDER := CreateCertDER(cname)
-
-	b := pem.Block{Type: "CERTIFICATE", Bytes: certDER}
-	certPEM = pem.EncodeToMemory(&b)
-	keyPEM = pem.EncodeToMemory(&pem.Block{
-		Type:  "RSA PRIVATE KEY",
-		Bytes: x509.MarshalPKCS1PrivateKey(privKey),
-	})
-
-	return
 }
